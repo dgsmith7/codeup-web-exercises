f03b6c395dbc1793d6e3bde51100f9d49ae6b14e
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-09-14 14:52:37 -0500

    completed math-js-circle

--- js/circle.js
+++ js/circle.js
@@ -2,14 +2,14 @@
     "use strict";
 
     // create a circle object
-    var circle = {
+    let circle = {
         radius: 3,
 
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return ((this.radius * this.radius) * Math.PI); // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
@@ -17,8 +17,14 @@
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
+            let result = 0;
+            if (doRounding) {
+                result = Math.round(this.getArea());
+            } else {
+                result = this.getArea();
+            }
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + result);
         }
     };
 
@@ -30,7 +36,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
