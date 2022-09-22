9f8b1500382ee0ef8981a81055336f73d36ab3ec
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-09-22 09:36:31 -0500

    completed warm-ups

--- js/bonus-exercises.js
+++ js/bonus-exercises.js
@@ -5,53 +5,104 @@ Reverse a String: Write a JS function that accepts a string and returns the stri
 
 (function() {
 
-    function areaOfTri(a, b, c) {
-        // Heron's formula
-        // https://byjus.com/maths/area-of-a-triangle/#Area-of-a-Triangle-Formula
-        let semiP = (a + b + c) / 2;
-        return Math.sqrt(semiP * (semiP - a) * (semiP - b) * (semiP - c));
-    }
+    // function areaOfTri(a, b, c) {
+    //     // Heron's formula
+    //     // https://byjus.com/maths/area-of-a-triangle/#Area-of-a-Triangle-Formula
+    //     let semiP = (a + b + c) / 2;
+    //     return Math.sqrt(semiP * (semiP - a) * (semiP - b) * (semiP - c));
+    // }
+    //
+    // function backwards(str) {
+    //     //https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+    //     //truns string into an array then uses array.reverse, then rejoins the array in to a string and returns it.
+    //     return str.split("").reverse().join("");
+    // }
+    //
+    // //FizzBuzz Activity: Print numbers 1-100. If # is divisible by 3 print Fizz. If # is divisible by 5 print Buzz. If # is divisible by both 3 and 5 print FizzBuzz
+    // // Shuffle an array: Write a function to shuffle an array to a completely and entirely random order.
+    // for (let i = 0; i < 100; i++) {
+    //     if ((i % 3 ==0) && (i % 5 == 0)){
+    //         console.log(i + " FizzBuzz")
+    //     } else
+    //     if (i % 3 == 0) {
+    //         console.log(i + " Fizz");
+    //     } else
+    //     if (i % 5 == 0) {
+    //         console.log(i + " Buzz");
+    //     } else {
+    //         console.log(i);
+    //     }
+    // }
+    //
+    // function shuffle(myArray) {
+    //     // below:  shuffle array - //from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    //     // <0 is a before b, >0 is a after b and 0 is keep same:  mathrand-0.5 will be between -.5 and .5
+    //     let currentValue = [...myArray];
+    //     return myArray.sort(function () {return Math.random() - 0.5;});
+    // }
+    //
+    // function shuffleArray(myArray) {
+    //     return myArray.sort(function () {return Math.random() - 0.5;});
+    // }
+    //
+    //
+    //     console.log(areaOfTri(10, 10, 3));
+    // console.log(backwards("codeup"));
+    // let myArray = [1, 2, 3, 4, 5];
+    // console.log("My array is - " + myArray);
+    // console.log("Shuffled version is - " + shuffle(myArray));
+    // console.log(shuffleArray(myArray));
+    // console.log(Math.random() - 0.5);
+    //
 
-    function backwards(str) {
-        //https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
-        //truns string into an array then uses array.reverse, then rejoins the array in to a string and returns it.
-        return str.split("").reverse().join("");
-    }
+/*
+Warm up:
+Create a function to sort the array from lowest to highest. Make sure the function accepts an array and
+returns an array.
+*/
 
-    //FizzBuzz Activity: Print numbers 1-100. If # is divisible by 3 print Fizz. If # is divisible by 5 print Buzz. If # is divisible by both 3 and 5 print FizzBuzz
-    // Shuffle an array: Write a function to shuffle an array to a completely and entirely random order.
-    for (let i = 0; i < 100; i++) {
-        if ((i % 3 ==0) && (i % 5 == 0)){
-            console.log(i + " FizzBuzz")
-        } else
-        if (i % 3 == 0) {
-            console.log(i + " Fizz");
-        } else
-        if (i % 5 == 0) {
-            console.log(i + " Buzz");
-        } else {
-            console.log(i);
-        }
-    }
+function sortArray(myArray) {
+    let newArray = [...myArray];
+    newArray.sort((a, b) => a - b);
+    return newArray;
+}
 
-    function shuffle(myArray) {
-        // below:  shuffle array - //from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-        // <0 is a before b, >0 is a after b and 0 is keep same:  mathrand-0.5 will be between -.5 and .5
-        let currentValue = [...myArray];
-        return myArray.sort(function () {return Math.random() - 0.5;});
-    }
-
-    function shuffleArray(myArray) {
-        return myArray.sort(function () {return Math.random() - 0.5;});
-    }
+let anArray = [1,5,7,8,2];
+console.log(sortArray(anArray));
+console.log(anArray);
 
 
-        console.log(areaOfTri(10, 10, 3));
-    console.log(backwards("codeup"));
-    let myArray = [1, 2, 3, 4, 5];
-    console.log("My array is - " + myArray);
-    console.log("Shuffled version is - " + shuffle(myArray));
-    console.log(shuffleArray(myArray));
-    console.log(Math.random() - 0.5);
+/*
+2. Write a function that returns the reading status of each of the following books:
+*/
+let library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games',
+        readingStatus: false
+    }];
+
+
+
+function showStatuses() {
+    let statuses = [];
+    library.forEach(element => statuses.push(element.readingStatus));
+    return statuses;
+}
+
+console.log(showStatuses());
+
+
+
 }());
 
