7db29b9cf112af56469237fb90ae4f00106bcc18
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-09-22 16:17:38 -0500

    Added practice problems to warmup-exercises

--- js/warmup-exercises.js
+++ js/warmup-exercises.js
@@ -75,32 +75,107 @@ console.log(anArray);
 /*
 2. Write a function that returns the reading status of each of the following books:
 */
-let library = [
-    {
-        author: 'Bill Gates',
-        title: 'The Road Ahead',
-        readingStatus: true
-    },
-    {
-        author: 'Steve Jobs',
-        title: 'Walter Isaacson',
-        readingStatus: true
-    },
-    {
-        author: 'Suzanne Collins',
-        title:  'Mockingjay: The Final Book of The Hunger Games',
-        readingStatus: false
-    }];
+/*
+    Core competencies assessed
+    Reading and following directions
+    Producing solutions that match the problem specification
+    Using Git and working with GitHub
+        Cloning a git repository from GitHub
+    Creating your own branch
+    Committing and pushing work to a new branch on GitHub
+    Programing fundamentals in JavaScript
+    Working with values, variables, and data types
+    Using assignment and comparision operators
+    Working with JS internal functions to make decisions and perform actions.
+        Using conditional logic to make decisions inside functions
+    Writing functions that take in inputs, process, and return outputs.
+        Here's some example word problems:
+// Define a function named timesFour that takes in an input and multiples that input by four if it is numeric. If the input is not numeric, then return false.
+*/
+    function timesFour(num) {
+        if ((isNaN(num)===true) || (typeof num === "boolean") || (num === null)) {
+            return false;
+        } else {
+            return num * 4.0;
+        }
+    }
+    console.log("should be 0 - " + timesFour(0));
+    console.log("should be 20 - " + timesFour("5"));
+    console.log("should be -20 - " + timesFour(-5));
+    console.log("should be false - " + timesFour('Texas'));
+    console.log("should be false - " + timesFour([2,1,0]));
+    console.log("should be false - " + timesFour(true));
+    console.log("should be false - " + timesFour(null));
+    /*
+// timesFour(0) //0
+// timesFour("5") //20
+// timesFour(-5) //-20
+// timesFour('Texas') //false
+// timesFour([2,1,0]) //false
+// timesFour(true) //false
+// timesFour(null) //false
+
+// Define a function named convertDaystoHours that takes in one input Days. Return the conversion of the number of days into total hours. If the input is not numeric or a numeric string, convertDaystoHours, should return false.
+*/
+    function converDaystoHours (days) {
+        if ((isNaN(days)===true) || (typeof days === "boolean") || (days === null)) {
+            return false
+        } else {
+            return days * 24.0;
+        }
+    }
+
+    console.log("should be 0 - " + converDaystoHours(0));
+    console.log("should be 120 - " + converDaystoHours("5"));
+    console.log("should be false - " + converDaystoHours('Texas'));
+    console.log("should be false - " + converDaystoHours([2,1,0]));
+    console.log("should be false - " + converDaystoHours(true));
+    console.log("should be false - " + converDaystoHours(false));
+
+    /*
+// convertDaystoHours(0) //0
+// convertDaystoHours('5') // 120
+// convertDaystoHours('Texas') //false
+// convertDaystoHours([2, 1, 0]) //false
+// convertDaystoHours(true) //false
+// convertDaystoHours(false) //false
+
+// Write a function named calculateTax that takes in two inputs representing the totalPaid and the taxPercent. If both inputs are numeric or numeric strings, calculateTax should return the total with tax added in a string looking like: “$XX.XX”. If either or both inputs are not numeric or numeric strings, calculateTax should return false.
+*/
+    function calculateTax(totalPaid, taxPercent) {
+        if (((isNaN(totalPaid)===true) || (typeof totalPaid === "boolean") || (totalPaid === null)) ||
+        ((isNaN(taxPercent)===true) || (typeof taxPercent === "boolean") || (taxPercent === null))) {
+            return false;
+        } else {
+            return "$" + (totalPaid + (totalPaid * (taxPercent/100.0))).toFixed(2);
+        }
+    }
+
+    console.log("should be $27.00 - " + calculateTax(25, 8));
+    console.log("should be $11.20 - " + calculateTax(10, 12));
+    console.log("should be $138.60 - " + calculateTax(120, 15.5));
+    console.log("should be false - " + calculateTax(10, true));
+    console.log("should be false - " + calculateTax([1,2,3], 10));
+    console.log("should be false - " + calculateTax("Codeup", 100));
+    console.log("should be false - " + calculateTax());
+/*
+// calculateTax(25, 8)               // “$27.00"
+// calculateTax(10, 12)             // “$11.20”
+// calculateTax(120, 15.5)           // “$138.60"
+// calculateTax(10, true)           // false
+// calculateTax([1, 2, 3], 10)      // false
+// calculateTax(“Codeup”, 100)      // false
+// calculateTax()                   // false
+*/
 
 
 
-function showStatuses() {
-    let statuses = [];
-    library.forEach(element => statuses.push(element.readingStatus));
-    return statuses;
-}
 
-console.log(showStatuses());
+
+
+
+
+
 
 
 
