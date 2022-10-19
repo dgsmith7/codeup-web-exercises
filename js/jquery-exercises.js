da7014885087c97998ff9f8b16b6e21b9a42f315
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-10-19 11:37:14 -0500

    complete jquery event handler piece

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,6 +1,18 @@
 "use strict";
 
 (function () {
-    $('hi, p, li').css('background-color', 'yellow');
-    alert($('h1').html());
+    $('h1').click(function () {
+        $(this).css('background-color', '#FF0');
+    });
+    $('p').dblclick(function (e) {
+        $(this).css('font-size', '18px');
+    });
+    $('li').hover(
+        function () {
+            $(this).css('color', '#F00');
+        },
+        function () {
+            $(this).css('color', '#000');
+        }
+    );
 }());
