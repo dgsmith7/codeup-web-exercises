d729c0b11f8821a08c4c41a7b95da32a2870b282
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-10-21 10:39:14 -0500

    added highlighting to selected question on FAQ project

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -2,34 +2,44 @@
 
 (function () {
     $('dt.i1').on('click', () => {
-        $('dd.i1').toggleClass('invisible')
+        $('dd.i1').toggleClass('invisible');
+        $('dt.i1').toggleClass('sel');
     })
     $('dt.i2').on('click', () => {
-        $('dd.i2').toggleClass('invisible')
+        $('dd.i2').toggleClass('invisible');
+        $('dt.i2').toggleClass('sel');
     })
     $('dt.i3').on('click', () => {
-        $('dd.i3').toggleClass('invisible')
+        $('dd.i3').toggleClass('invisible');
+        $('dt.i3').toggleClass('sel');
     })
     $('dt.i4').on('click', () => {
-        $('dd.i4').toggleClass('invisible')
+        $('dd.i4').toggleClass('invisible');
+        $('dt.i4').toggleClass('sel');
     })
     $('dt.i5').on('click', () => {
-        $('dd.i5').toggleClass('invisible')
+        $('dd.i5').toggleClass('invisible');
+        $('dt.i5').toggleClass('sel');
     })
     $('dt.i6').on('click', () => {
-        $('dd.i6').toggleClass('invisible')
+        $('dd.i6').toggleClass('invisible');
+        $('dt.i6').toggleClass('sel');
     })
     $('dt.i7').on('click', () => {
-        $('dd.i7').toggleClass('invisible')
+        $('dd.i7').toggleClass('invisible');
+        $('dt.i7').toggleClass('sel');
     })
     $('dt.i8').on('click', () => {
-        $('dd.i8').toggleClass('invisible')
+        $('dd.i8').toggleClass('invisible');
+        $('dt.i8').toggleClass('sel');
     })
     $('dt.i9').on('click', () => {
-        $('dd.i9').toggleClass('invisible')
+        $('dd.i9').toggleClass('invisible');
+        $('dt.i9').toggleClass('sel');
     })
     $('dt.i10').on('click', () => {
-        $('dd.i10').toggleClass('invisible')
+        $('dd.i10').toggleClass('invisible');
+        $('dt.i10').toggleClass('sel');
     })
 
 }());
