3f3c2365884643dc1c73f573e6b20dd28086ba28
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-10-21 14:07:45 -0500

    finished image swap bonus on faq

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,6 +1,11 @@
 "use strict";
 
 (function () {
+
+    let card1;
+    let card2;
+    let card3;
+
     $('dt.i1').on('click', () => {
         $('dd.i1').toggleClass('invisible');
         $('dt.i1').toggleClass('sel');
@@ -41,5 +46,53 @@
         $('dd.i10').toggleClass('invisible');
         $('dt.i10').toggleClass('sel');
     })
+    $('#hilite-last-li').on('click', function (index) {
+        $('ul').each(function () {
+            $(this).children().last().toggleClass('sel');
+        });
+    })
+    $('h3').on('click', function (index) {
+        $(this).next().toggleClass('li-h3-clicked');
+    })
+    $('li').on('click', function () {
+        $(this).parent().children().first().toggleClass('selected-li');
+    })
 
+    function setCardAssignments() {
+        card1 = $('#card-1').html();
+        card2 = $('#card-2').html();
+        card3 = $('#card-3').html();
+    }
+
+    function swapLeft() {
+        setCardAssignments();
+        let temp = card1;
+        $('#card-1').html(card2);
+        $('#card-2').html(card3);
+        $('#card-3').html(temp);
+    }
+
+    function swapRight() {
+        setCardAssignments();
+        let temp = card1;
+        $('#card-1').html(card3);
+        $('#card-3').html(card2);
+        $('#card-2').html(temp);
+    }
+
+    $('#card-1').on('click', function () { // The left frame swaps to the right and takes the image from the frame in the center.
+        swapLeft();
+    });
+
+    $('#card-2').on('click', function () { // The center frame swaps randomly to either the left or right.
+        if (Math.random() >= 0.5) {
+            swapLeft();
+        } else {
+            swapRight();
+        }
+    });
+
+    $('#card-3').on('click', function () {  // The right frame swaps to the left and takes the image from the frame in the center.
+        swapRight();
+    });
 }());
