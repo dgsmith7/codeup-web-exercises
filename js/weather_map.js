94a9cdb42fc6ebffa4752c6a7d193c9744d0b989
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2022-10-29 19:05:53 -0500

    finished up population of display area.

--- js/weather_map.js
+++ js/weather_map.js
@@ -56,7 +56,6 @@ accessibility?
         $.get(url).done(function (data) {// once we have lat lon from zip code get the local area name
             wx = data;  // assign file contents to global variable
             timeShift = wx.timezone / 3600;
-            console.log("weather: ", wx)
             lookUpLocationNameByLatLon(location.lat, location.lon);
         }).fail(function (jqXhr, status, error) {
             alert("There was an error getting local conditions! Check the console for details");
@@ -68,7 +67,6 @@ accessibility?
     function lookUpLocationNameByLatLon(lat, lon) {
         let url = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=5&appid=${OPEN_WX_MAP_KEY}`
         $.get(url).done(function (data) { // now that we have lat lon, local wx, and local name from zip, update page
-            console.log("name lookup:", data);
             locName = data[0].name + ", " + data[0].state;
             getFiveDayData();//getSunAndMoonData();//apiCall();//
         }).fail(function (jqXhr, status, error) {
@@ -80,11 +78,9 @@ accessibility?
 
     function getFiveDayData() {  // use ajax to get restaurant dat from file
         let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${location.lat}&lon=${location.lon}&units=imperial&appid=${OPEN_WX_MAP_KEY}`
-        //console.log(url);
         $.get(url).done(function (data) {// once we have lat lon from zip code get the local area name
             fiveDay = data;  // assign file contents to global variable
             timeShift = wx.timezone / 3600;
-            console.log("forecast: ", fiveDay)
             populateDisplay();
         }).fail(function (jqXhr, status, error) {
             alert("There was an error getting forecast! Check the console for details");
@@ -94,7 +90,13 @@ accessibility?
     }
 
     function populateDisplay() {
-        createMap();
+        if (!map) {
+            createMap();
+        }
+
+        let iconDescObj;
+        let tempObj;
+        let windObj;
         let imgURL = `./assets/images/weather/${fileMap.get(wx.weather[0].icon)}`;
         //  if it is night sub moon for sun: imgURL something
         setDayNightBG();
@@ -106,6 +108,47 @@ accessibility?
         $('#current-humid').html(`Humidity: <em>${wx.main.humidity}%</em>`);
         $('#current-wind').html(`Winds:<br/><em>${formatDirection(wx.wind.deg)} at ${(wx.wind.speed).toFixed(0)} knots</em>`);
         $('#current-baro').html(`Pressure:<br/>hPa: <em>${wx.main.pressure}<br/>Inches Hg: ${(wx.main.pressure / 33.864).toFixed(2)}</em>`);
+
+        iconDescObj = computeForecastIcon(fiveDay.list.slice(0, 8), fiveDay.city.sunrise);
+        tempObj = computeForecastTemps(fiveDay.list.slice(0, 8));
+        windObj = computerForecastWinds(fiveDay.list.slice(0, 8));
+        $('#day-one-img').attr("src", `./assets/images/weather/${fileMap.get(iconDescObj.icon)}`);
+        $('#day-one-date').html(`<b>${convertDate(fiveDay.list[5].dt)}</b>`);
+        $('#day-one-desc').html(`<em>${iconDescObj.description}</em>`);
+        $('#day-one-temps').html(`Temps:<br/>Low: <em>${tempObj.lo} F</em><br/>High: <em>${tempObj.hi} F</em>`);
+        $('#day-one-wind').html(`Winds:<br/><em>${windObj.deg} at ${windObj.speed} knots</em>`);
+
+        iconDescObj = computeForecastIcon(fiveDay.list.slice(8, 17), fiveDay.city.sunrise);
+        windObj = computerForecastWinds(fiveDay.list.slice(8, 17));
+        $('#day-two-img').attr("src", `./assets/images/weather/${fileMap.get(iconDescObj.icon)}`);
+        $('#day-two-date').html(`<b>${convertDate(fiveDay.list[13].dt)}</b>`);
+        $('#day-two-desc').html(`<em>${iconDescObj.description}</em>`);
+        $('#day-two-temps').html(`Temps:<br/>Low: <em>${tempObj.lo} F</em><br/>High: <em>${tempObj.hi} F</em>`);
+        $('#day-two-wind').html(`Winds:<br/><em>${windObj.deg} at ${windObj.speed} knots</em>`);
+
+        iconDescObj = computeForecastIcon(fiveDay.list.slice(16, 25), fiveDay.city.sunrise);
+        windObj = computerForecastWinds(fiveDay.list.slice(16, 25));
+        $('#day-three-img').attr("src", `./assets/images/weather/${fileMap.get(iconDescObj.icon)}`);
+        $('#day-three-date').html(`<b>${convertDate(fiveDay.list[21].dt)}</b>`);
+        $('#day-three-desc').html(`<em>${iconDescObj.description}</em>`);
+        $('#day-three-temps').html(`Temps:<br/>Low: <em>${tempObj.lo} F</em><br/>High: <em>${tempObj.hi} F</em>`);
+        $('#day-three-wind').html(`Winds:<br/><em>${windObj.deg} at ${windObj.speed} knots</em>`);
+
+        iconDescObj = computeForecastIcon(fiveDay.list.slice(24, 33), fiveDay.city.sunrise);
+        windObj = computerForecastWinds(fiveDay.list.slice(24, 33));
+        $('#day-four-img').attr("src", `./assets/images/weather/${fileMap.get(iconDescObj.icon)}`);
+        $('#day-four-date').html(`<b>${convertDate(fiveDay.list[29].dt)}</b>`);
+        $('#day-four-desc').html(`<em>${iconDescObj.description}</em>`);
+        $('#day-four-temps').html(`Temps:<br/>Low: <em>${tempObj.lo} F</em><br/>High: <em>${tempObj.hi} F</em>`);
+        $('#day-four-wind').html(`Winds:<br/><em>${windObj.deg} at ${windObj.speed} knots</em>`);
+
+        iconDescObj = computeForecastIcon(fiveDay.list.slice(32, 40), fiveDay.city.sunrise);
+        windObj = computerForecastWinds(fiveDay.list.slice(32, 40));
+        $('#day-five-img').attr("src", `./assets/images/weather/${fileMap.get(iconDescObj.icon)}`);
+        $('#day-five-date').html(`<b>${convertDate(fiveDay.list[37].dt)}</b>`);
+        $('#day-five-desc').html(`<em>${iconDescObj.description}</em>`);
+        $('#day-five-temps').html(`Temps:<br/>Low: <em>${tempObj.lo} F</em><br/>High: <em>${tempObj.hi} F</em>`);
+        $('#day-five-wind').html(`Winds:<br/><em>${windObj.deg} at ${windObj.speed} knots</em>`);
     }
 
     function convertDate(epoch) {
@@ -125,28 +168,28 @@ accessibility?
     function formatDirection(dir) {
         let dirStr = "";
         if ((dir >= 0 && dir <= 22) || (dir >= 337)) {
-            dirStr = "N"
+            dirStr = "North"
         }
         if (dir >= 23 && dir <= 66) {
-            dirStr = "NE"
+            dirStr = "Northeast"
         }
         if (dir >= 67 && dir <= 112) {
-            dirStr = "E"
+            dirStr = "East"
         }
         if (dir >= 113 && dir <= 157) {
-            dirStr = "SE"
+            dirStr = "Southeast"
         }
         if (dir >= 158 && dir <= 202) {
-            dirStr = "S"
+            dirStr = "South"
         }
         if (dir >= 203 && dir <= 247) {
-            dirStr = "SW"
+            dirStr = "Southwest"
         }
         if (dir >= 248 && dir <= 292) {
-            dirStr = "W"
+            dirStr = "West"
         }
         if (dir >= 293 && dir <= 336) {
-            dirStr = "NW"
+            dirStr = "Northwest"
         }
         return dirStr;
     }
@@ -178,7 +221,6 @@ accessibility?
             map.resize();
         }).on('moveend', () => {  // event to tell when goecoder done moving map
             if (dragFlag === true) {  //  if map event from dragging marker just reset flag
-                console.log('reset dragging flag');
                 dragFlag = false;
             } else {  // otherwise get new location and do stuff
                 let nc = map.getCenter();
@@ -187,7 +229,6 @@ accessibility?
                 };
                 if (gcFlag === true) {
                     gcFlag = false;
-                    console.log(map);
                     marker
                         .setLngLat([location.lon, location.lat])
                         .addTo(map);
@@ -232,13 +273,50 @@ accessibility?
         dragFlag = true;
         let reposition = marker.getLngLat();
         location = {"lat": reposition.lat, "lon": reposition.lng};
-        console.log("dragged - ", location);
         marker
             .setLngLat([location.lon, location.lat])
             .addTo(map);
+        map.easeTo({center: [location.lon, location.lat]});
         getLocalWxData();
     }
 
-    console.log(location);
+    function computeForecastIcon(arr, sunrise) {
+        let obj = {icon: '02d', description: 'few clouds'};
+        for (let i = 0; i < 8; i++) {
+            if (arr[i].dt > sunrise) {
+                obj.icon = arr[i].weather[0].icon;
+                obj.description = arr[i].weather[0].description;
+                break;
+            }
+        }
+        return obj;
+    }
+
+    function computeForecastTemps(arr) {
+        let tempObj = {'lo': arr[0].main.temp_min, 'hi': arr[0].main.temp_max};
+        let loSum = 0;
+        let hiSum = 0
+        for (let i = 0; i < 8; i++) {
+            loSum += arr[i].main.temp_min;
+            hiSum += arr[i].main.temp_max;
+        }
+        tempObj.lo = Math.floor(loSum / 8);
+        tempObj.hi = Math.floor(hiSum / 8);
+        return tempObj;
+    }
+
+    function computerForecastWinds(arr) {
+        let windObj = {'deg': arr[0].wind.deg, 'speed': arr[0].wind.speed};
+        let degSum = 0;
+        let spdSum = 0
+        for (let i = 0; i < 8; i++) {
+            degSum += arr[i].wind.deg;
+            spdSum += arr[i].wind.speed;
+        }
+        windObj.deg = formatDirection(Math.floor(degSum / 8));
+        windObj.speed = Math.floor(spdSum / 8);
+        return windObj;
+    }
+
     getFileMap();
 }());
